0981c37e5a2cad96d7be8cdb1c35cf773e05fe94
speier <kalman.speier@gmail.com>
2018-12-02 23:35:18 +0100

    basic elem recycle

--- pkg/dom/render.go
+++ pkg/dom/render.go
@@ -1,24 +1,24 @@
 package dom
 
 import (
+	"strings"
 	"syscall/js"
 
 	"github.com/speier/gowasm/pkg/vdom"
 )
 
 var (
-	Window   = js.Global().Get("window")
-	Document = js.Global().Get("document")
+	document = js.Global().Get("document")
+	_oldNode *vdom.VNode
 )
 
 func QuerySelector(selector string) js.Value {
-	return Document.Call("querySelector", selector)
+	return document.Call("querySelector", selector)
 }
 
-var oldNode *vdom.VNode
-
 func Render(node *vdom.VNode, container js.Value) {
-	oldNode = Patch(oldNode, node, container)
+	recycle(container)
+	_oldNode = Patch(_oldNode, node, container)
 }
 
 func Patch(oldNode *vdom.VNode, newNode *vdom.VNode, container js.Value) *vdom.VNode {
@@ -54,10 +54,10 @@ func changed(a *vdom.VNode, b *vdom.VNode) bool {
 
 func createElement(node *vdom.VNode) js.Value {
 	if node.Type == vdom.TextNode {
-		return Document.Call("createTextNode", node.TagName)
+		return document.Call("createTextNode", node.TagName)
 	}
 
-	el := Document.Call("createElement", node.TagName)
+	el := document.Call("createElement", node.TagName)
 
 	if node.Attrs != nil {
 		if node.Attrs.Props != nil {
@@ -79,4 +79,33 @@ func createElement(node *vdom.VNode) js.Value {
 	}
 
 	return el
+}
+
+func recycle(container js.Value) {
+	if _oldNode != nil {
+		return
+	}
+
+	el := container.Get("childNodes").Index(0)
+	if el.Type() == js.TypeUndefined {
+		return
+	}
+
+	_oldNode = &vdom.VNode{
+		TagName: strings.ToLower(el.Get("nodeName").String()),
+	}
+	childNodes := el.Get("childNodes")
+	childLen := childNodes.Length()
+	for i := 0; i < childLen; i++ {
+		c := childNodes.Index(i)
+		if c.Get("nodeType").Int() == 3 {
+			// text node
+			_oldNode.Children = append(_oldNode.Children, &vdom.VNode{
+				Type:    vdom.TextNode,
+				TagName: c.Get("nodeValue").String(),
+			})
+		} else {
+			recycle(c)
+		}
+	}
 }
